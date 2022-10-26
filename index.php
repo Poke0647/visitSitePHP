fa2156aa69e4d54daa8489f8aed4f4d60eec59b8
Poke0647 <pokedbrain@gmail.com>
2022-10-27 04:16:39 +1000

    v1.0.1.2
    Almost everything is working

--- index.php
+++ index.php
@@ -1,31 +1,32 @@
 <?php
 
-include ("./general/head1.php");
+include "./general/head1.php";
 
-$p = "main";
-$p = $_GET['p'];
-  
+//$p = 0;
+$p = $_GET['p'];  
 
-if ($p == "main") {
-  include ("./main/head_m.php");
-} elseif ($p == 'port') {
-  include ("./port/head_p.php");
-} elseif ($p == 'cont') {
-  include ("./cont/head_c.php");
+if ($p == 0) {
+  include "./main/head_m.php";
+}
+elseif ($p == 1) {
+  include "./port/head_p.php";
+}
+elseif ($p == 2) {
+  include "./cont/head_c.php";
 }
 
-include ("./general/header.php");
+include "./general/header.php";
 
-if($p == 'main'){
-  include ("./main/subheader_m.php");
-  include ("./main/main_m.php");
-} elseif ($p == 'port') {
-  include ("./port/subheader_p.php");
-  include ("./port/php/main_p.php");
-} elseif ($p == 'cont') {
-  include ("./contacts/subheader_c.php");
-  include ("./contacts/main_c.php");
+if($p == 0){
+  include "./main/subheader_m.php";
+  include "./main/main_m.php";
+} elseif ($p == 1) {
+  include "./port/subheader_p.php";
+  include "./port/main_p.php";
+} elseif ($p == 2) {
+  include "./contacts/subheader_c.php";
+  include "./contacts/main_c.php";
 }
 
-include ("./general/footer.php");
+include "./general/footer.php";
 ?>
