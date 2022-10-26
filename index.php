e84daaed9968fb549a8754cbf5db0deea86c9e21
Poke0647 <pokedbrain@gmail.com>
2022-10-27 01:38:45 +1000

    v1.0.1.1

--- index.php
+++ index.php
@@ -1,33 +1,31 @@
 <?php
 
-include "./general/head1.php";
+include ("./general/head1.php");
 
 $p = 'main';
 $p = $_GET['p'];
   
 
 if ($p == 'main') {
-  include "./main/head_m.php";
-}
-elseif ($p == 'port') {
-  include "./port/head_p.php";
-}
-elseif ($p == 'cont') {
-  include "./cont/head_c.php";
+  include ("./main/head_m.php");
+} elseif ($p == 'port') {
+  include ("./port/head_p.php");
+} elseif ($p == 'cont') {
+  include ("./cont/head_c.php");
 }
 
-include "./general/header.php";
+include ("./general/header.php");
 
 if($p == 'main'){
-  include "./main/subheader_m.php";
-  include "./main/main_m.php";
+  include ("./main/subheader_m.php");
+  include ("./main/main_m.php");
 } elseif ($p == 'port') {
-  include "./port/subheader_p.php";
-  include "./port/php/main_p.php";
+  include ("./port/subheader_p.php");
+  include ("./port/php/main_p.php");
 } elseif ($p == 'cont') {
-  include "./contacts/subheader_c.php";
-  include "./contacts/main_c.php";
+  include ("./contacts/subheader_c.php");
+  include ("./contacts/main_c.php");
 }
 
-include "./general/footer.php";
+include ("./general/footer.php");
 ?>
