cb8c9f4b628c09ae9044952c24b38ad8d662b893
Pokkeee <pokedbrain@gmail.com>
2022-10-28 13:52:31 +1000

    minor updates

--- index.php
+++ index.php
@@ -8,11 +8,9 @@ $p = $_GET['p'];
 
   if ($p == 'main') {
     include "main/head_m.php";
-  }
-  elseif ($p == 'port') {
+  } elseif ($p == 'port') {
     include "port/head_p.php";
-  }
-  elseif ($p == 'cont') {
+  } elseif ($p == 'cont') {
     include "cont/head_c.php";
   }
 
@@ -21,10 +19,12 @@ include "general/header.php";
 if($p == 'main'){
   include "main/subheader_m.php";
   include "main/main_m.php";
-} elseif ($p == 'port') {
+} 
+elseif ($p == 'port') {
   include "port/subheader_p.php";
   include "port/php/main_p.php";
-} elseif ($p == 'cont') {
+} 
+elseif ($p == 'cont') {
   include "contacts/subheader_c.php";
   include "contacts/main_c.php";
 }
