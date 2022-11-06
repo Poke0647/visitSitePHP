81a56513ecc9ed0e31ce81671242ad42e9ee9507
Poke0647 <pokedbrain@gmail.com>
2022-11-06 20:17:56 +1000

    1

--- login/login.php
+++ login/login.php
@@ -2,8 +2,8 @@
     <div class="loginForm">
         <h1>Вход</h1> 
         <form name="login" method="post" action="login.php">
-            <label>Логин <input type="text" name="login"></label><br>
-            <label>Пароль <input type="text" name="passwd"></label>
+            <input type="text" name="login"><br>
+            <input type="password" name="passwd">
         </form>
         <a href="index.php?loginn=0">Отмена</a>
     </div>
