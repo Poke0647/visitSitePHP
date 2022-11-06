6e5f6ddaefd6d1a9325dfae33ed5019a6f0bad03
Poke0647 <pokedbrain@gmail.com>
2022-11-07 01:06:32 +1000

    1

--- login/login.php
+++ login/login.php
@@ -4,7 +4,7 @@
         <form name="login" method="post" action="login.php">
             <input type="text" placeholder="Логин" name="login"><br>
             <input type="password" placeholder="Пароль" name="passwd"><br>
-            <input type="submit" placeholder="Вход" name="send">
+            <input type="submit" value="Вход" name="send">
         </form>
         <a href="index.php?loginn=0">Назад</a>
     </div>
