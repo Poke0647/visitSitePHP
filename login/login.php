1ae15b1ad4ce84e4622e8b476428d8e0f26da21e
Poke0647 <pokedbrain@gmail.com>
2022-11-07 00:36:31 +1000

    Update login.php

--- login/login.php
+++ login/login.php
@@ -3,7 +3,7 @@
         <h1>Вход</h1> 
         <form name="login" method="post" action="login.php">
             <input type="text" name="login"><br>
-            <input type="password" name="passwd">
+            <input type="password" name="passwd"><br>
             <a><input type="submit" name="send"></a>
         </form>
         <a href="index.php?loginn=0">Назад</a>
