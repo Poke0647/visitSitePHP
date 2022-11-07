96fb9789bbc18255bd09349f0ce6ee883aa3f913
Poke0647 <pokedbrain@gmail.com>
2022-11-08 00:27:07 +1000

    Update login.php

--- login/login.php
+++ login/login.php
@@ -1,7 +1,7 @@
 <div class="loginFormBg">
     <div class="loginForm">
         <h1>Вход</h1> 
-        <form name="login" method="post" action="login.php">
+        <form name="login" method="POST" action="login.php">
             <input class="inp" type="text" placeholder="Логин" name="login"><br>
             <input class="inp" type="password" placeholder="Пароль" name="passwd"><br>
             <input type="submit" value="Вход" name="send">
