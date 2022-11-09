f481f020966b720c1691df31fa76aae32fb6ceec
Pokkeee <pokedbrain@gmail.com>
2022-11-09 14:12:00 +1000

    Update login.php

--- login/login.php
+++ login/login.php
@@ -11,7 +11,7 @@
             $passwd = CRYPT($_POST["passwd"]);
             echo "login: $login <br> password: $passwd";
 
-            if (mysql_connect("95.154.67.183", "pavelisaenko_xyz_login", "George_Orwel_1984", "visit_site_db")){
+            if (mysql_connect("95.154.67.183", "pavelisaenko_xyz_login", "George_Orwel_1984", "visit_site_db") == true){
                 echo "Connection with database established!";
             } else {
                 echo "Error: Can not connect to database! " .mysql_connect_error();
