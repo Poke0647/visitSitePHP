49e2409ade3149c74e82d76db886d42e6a3bb3da
Pokkeee <pokedbrain@gmail.com>
2022-11-09 17:18:05 +1000

    1

--- login/login.php
+++ login/login.php
@@ -11,7 +11,12 @@
             $passwd = CRYPT($_POST["passwd"]);
             echo "login: $login <br> password: $passwd";
 
-            
+            if (mysqli_connect("95.154.67.183", "pavelisaenko_xyz_login", "George_Orwel_1984", "visit_site_db") == true){
+                echo "Connection with database established!";
+              } else {
+                echo "Error: Can not connect to database! " .mysqli_connect_error();
+              }
+              
 
         }
         
