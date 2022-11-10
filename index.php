8619c0d3579f8038b59898ee16bf39d8c49763fc
Poke0647 <pokedbrain@gmail.com>
2022-11-11 01:16:35 +1000

    Update index.php

--- index.php
+++ index.php
@@ -7,12 +7,10 @@ $p = $_GET['p'];
 //Данный кусок вместе с session_start(); позволяет после вызова login.php
 // и выхода из него оставаться на странице, которая была только что активна
 //--------------
-
 $loginn = $_GET['loginn'];
 if(isset($_SESSION['p'])){
   if (isset($loginn)){
     $p = $_SESSION['p'];
-    
   }
 }
 
@@ -45,15 +43,13 @@ if (isset($_POST["login"]) and isset($_POST["passwd"])){
   $passwd = CRYPT($_POST["passwd"]);
   echo "login: $login <br> password: $passwd <br>";
   $username = $login;
-  
 
   $link = new mysqli("95.154.67.183", "pavelisaenko_xyz_login", "George_Orwel_1984", "visit_site_db");
 
   if ($link == true){
-      echo "Connection with database established!";
-      
+    echo "Connection with database established!"; 
   } else {
-      echo "Error: " .mysqli_connect_error();
+    echo "Error: " .mysqli_connect_error();
   }
 
   $_SESSION['username'] = $username;
